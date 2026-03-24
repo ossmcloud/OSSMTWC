303cba0d6de52cc23428ff6f4a93f1d6b213c345
ossmcloud <development@ossmcloud.ie>
2026-03-24 13:08:08 +0000

    inventory feature scaffolding (claude)

---
+++ FileCabinet/SuiteScripts/OSSMTWC/ui/js/oTWC_inventory.js
@@ -0,0 +1,51 @@
+/**
+ * @NApiVersion 2.1
+ * @NModuleScope public
+ * @NAmdConfig  /SuiteBundles/Bundle 548734/O/config.json
+ */
+define(['SuiteBundles/Bundle 548734/O/core.js', 'SuiteBundles/Bundle 548734/O/core.sql.js', './oTWC_pageBase.js', '../../O/oTWC_dialogEx.js', '../../data/oTWC_config.js'],
+    (core, coreSql, twcPageBase, dialog, twcConfig) => {
+
+
+        class TWCInventoryPage extends twcPageBase.TWCPageBase {
+            #changes = {};
+            constructor() {
+                super({ scriptId: 'otwc_inventory_sl' });
+
+            }
+
+            initPage() {
+
+            }
+
+            async onSave() {
+                try {
+                    this.wait();
+
+                    if (!this.dirty) { throw new Error('The record has not changed'); }
+
+                    var payload = this.#changes;
+
+                    await this.post({ action: 'save' }, payload);
+                    this.dirty = false;
+                    location.href = location.href.replace('&edit=T', '');
+
+                } catch (error) {
+                    await dialog.errorAsync(error);
+                } finally {
+                    this.waitClose();
+                }
+
+            }
+
+        }
+
+        return {
+
+            init: function () {
+                twcPageBase.init(new TWCInventoryPage())
+            }
+
+
+        }
+    });
---
+++ FileCabinet/SuiteScripts/OSSMTWC/ui/oTWC_inventory_sl.js
@@ -0,0 +1,41 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+
+ */
+define(['SuiteBundles/Bundle 548734/O/core.js', 'SuiteBundles/Bundle 548734/O/core.date.js', 'SuiteBundles/Bundle 548734/O/core.sql.js', 'SuiteBundles/Bundle 548734/O/ui/nsSuitelet.js', './views/oTWC_baseView.js', '../data/oTWC_config.js', '../ui/modules/oTWC_inventoryUtils.js', '../O/controls/oTWC_ui_fieldPanel.js'],
+    function (core, cored, coreSql, uis, twcBaseView, twcConfig, twcInventoryUtils, twcUIPanel) {
+        var PAGE_VERSION = 'v0.01';
+
+        var suiteLet = uis.new({ title: 'TWC Inventory', script: 'SuiteScripts/OSSMTWC/ui/oTWC_inventory_cs.js' });
+        suiteLet.get = (context, s) => {
+            var pageData = twcBaseView.initPageData(context);
+
+            var html = twcBaseView.initView(PAGE_VERSION, pageData, 'oTWC_inventory');
+
+            var readOnly = context.request.parameters.edit != 'T';
+            // @@NOTE: if permission lvl is 1 it means view only so even if parameter passed force to read only
+            if (pageData.userInfo.permission.lvl == 1) { readOnly = true; }
+
+            html = html.replace('{INVENTORY_DETAILS}', 'hello, dude...')
+
+            s.form.fieldHtml(html);
+        };
+
+        suiteLet.post = (context, s) => {
+            var userInfo = twcConfig.userInfo(context);
+            if (context.request.parameters.action == 'save') {
+                var payload = JSON.parse(context.request.body);
+                // @@TODO: INVENTORY: implement save
+                return { status: 'success' };
+            } else {
+                throw new Error(`Invalid post action: ${context.request.parameters.action || 'NO ACTION'}`);
+            }
+
+        };
+
+
+        return {
+            onRequest: uis.onRequest
+        }
+    });
