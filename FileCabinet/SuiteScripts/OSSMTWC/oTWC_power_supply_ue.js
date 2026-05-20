c76a098dbcd628935316ca9939a49261634a7c67
Javeed-22 <jsyed.workmail@gmail.com>
2026-05-20 15:44:43 +0530

    Minor changes on Power Supply.

--- FileCabinet/SuiteScripts/OSSMTWC/oTWC_power_supply_ue.js
+++ FileCabinet/SuiteScripts/OSSMTWC/oTWC_power_supply_ue.js
@@ -8,20 +8,13 @@ define([], () => {
     const beforeLoad = (context) => {
 
         try {
+            if (context.type !== context.UserEventType.EDIT) return;
             const form = context.form;
             form.clientScriptModulePath = './oTWC_power_supply_cs.js';
-            form.addButton({
-                id: 'custpage_select_gen_backup',
-                label: 'Select Generator Backup',
-                functionName: 'openGeneratorBackupSelector'
-            });
+            form.addButton({ id: 'custpage_select_gen_backup', label: 'Select Generator Backup', functionName: 'openGeneratorBackupSelector' });
 
         } catch (e) {
-
-            log.error(
-                'beforeLoad Error',
-                e
-            );
+            log.error('beforeLoad Error', e);
         }
     };
 
