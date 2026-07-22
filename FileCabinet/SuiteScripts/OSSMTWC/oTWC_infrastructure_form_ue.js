f91b4ccfc6f0b9ba8fad71943332ff8c59f100f3
Javeed-22 <jsyed.workmail@gmail.com>
2026-07-22 15:40:48 +0530

    Made changes on Infrastructure forms to fetch the details automatically.

--- FileCabinet/SuiteScripts/OSSMTWC/oTWC_infrastructure_form_ue.js
+++ FileCabinet/SuiteScripts/OSSMTWC/oTWC_infrastructure_form_ue.js
@@ -4,15 +4,33 @@
  * @NModuleScope public
  * @NAmdConfig  /SuiteBundles/Bundle 548734/O/config.json
  */
-define(['N/runtime', 'N/ui/serverWidget', 'N/redirect', 'SuiteBundles/Bundle 548734/O/core.js', 'O/form', './data/oTWC_infrastructure.js'],
-    (runtime, serverWidget, redirect, core, oui, twcInfra) => {
-
-        const VALID_FORMS = new Set([260, 263, 264, 265]);
+define(['N/runtime', 'N/ui/serverWidget', 'N/redirect', '/.bundle/548734/O/core.sql.js', 'SuiteBundles/Bundle 548734/O/core.js', 'O/form', './data/oTWC_infrastructure.js'],
+    (runtime, serverWidget, redirect, coreSQL, core, oui, twcInfra) => {
 
         const isUserInterface = () =>
             runtime.executionContext === runtime.ContextType.USER_INTERFACE;
 
         const isCreate = (type) => type === 'create';
+
+        function getInfrastructureFormMap() {
+
+            const infraToForm = {};
+            const formToInfra = {};
+            const validForms = new Set();
+            const rows = coreSQL.run(`
+                SELECT id, custrecord_twc_infra_type_form FROM customrecord_twc_infra_type
+                WHERE isinactive = 'F' AND custrecord_twc_infra_type_form IS NOT NULL
+            `);
+
+            rows.forEach(({ id, custrecord_twc_infra_type_form }) => {
+                const infraId = Number(id);
+                const formId = Number(custrecord_twc_infra_type_form);
+                infraToForm[infraId] = formId;
+                formToInfra[formId] = infraId;
+                validForms.add(formId);
+            });
+            return { infraToForm, formToInfra, validForms };
+        }
 
         function hideAllInfrastructureFields(form) {
             const fieldsToHide = new Set(['name', 'isinactive', ...Object.values(twcInfra.Fields)]);
@@ -23,26 +41,19 @@ define(['N/runtime', 'N/ui/serverWidget', 'N/redirect', 'SuiteBundles/Bundle 548
             }
         }
 
-        function handleCustomForm(rec, customFormId) {
+        function handleCustomForm(rec, customFormId, formMap) {
 
-            const infraTypeId = twcInfra.Fields.INFRASTRUCTURE_TYPE;
-            const formToInfraMap = { 260: 3, 263: 1, 264: 2, 265: 4 };
-            const infraValue = formToInfraMap[Number(customFormId)];
-            if (!infraValue) return;
-            rec.setValue({
-                fieldId: infraTypeId,
-                value: infraValue
-            });
+            const infraType = formMap.formToInfra[Number(customFormId)]
+            if(!infraType) return;
+            rec.setValue({ fieldId: twcInfra.Fields.INFRASTRUCTURE_TYPE, value: infraType });
         }
 
-        function enforceCorrectForm(context, rec) {
+        function enforceCorrectForm(context, rec, formMap) {
             const request = context.request;
             if (!request) return;
             const infraType = Number(rec.getValue({ fieldId: twcInfra.Fields.INFRASTRUCTURE_TYPE }));
             if (!infraType) return;
-            // Map infra type → form
-            const FORM_MAP = { 3: 260, 1: 263, 2: 264, 4: 265 };
-            const expectedFormId = FORM_MAP[infraType];
+            const expectedFormId = formMap.infraToForm[infraType];
             if (!expectedFormId) return;
 
             const currentCf = Number(request.parameters.cf);
@@ -52,17 +63,13 @@ define(['N/runtime', 'N/ui/serverWidget', 'N/redirect', 'SuiteBundles/Bundle 548
             let query = '';
             for (const key in params) {
                 if (key === 'cf') continue; // overwrite cf
-                query += (query ? '&' : '') +
-                    encodeURIComponent(key) + '=' +
-                    encodeURIComponent(params[key]);
+                query += (query ? '&' : '') + encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
             }
             // Add correct cf
             query += (query ? '&' : '') + 'cf=' + expectedFormId;
             const baseUrl = request.url.split('?')[0];
             const redirectUrl = baseUrl + '?' + query;
-            redirect.redirect({
-                url: redirectUrl
-            });
+            redirect.redirect({ url: redirectUrl });
         }
 
         function beforeLoad(context) {
@@ -70,27 +77,26 @@ define(['N/runtime', 'N/ui/serverWidget', 'N/redirect', 'SuiteBundles/Bundle 548
                 if (!isUserInterface()) return;
                 const { type, newRecord: rec, form: nsForm, request } = context;
                 const form = oui.get(nsForm);
+                const formMap = getInfrastructureFormMap();
                 const customFormId = Number(rec.getValue({ fieldId: 'customform' }));
                 // Make Infrastructure Type Field Inline
                 const fieldObj = form.f.getField({ id: twcInfra.Fields.INFRASTRUCTURE_TYPE });
-                fieldObj?.updateDisplayType({
-                    displayType: serverWidget.FieldDisplayType.INLINE
-                });
+                fieldObj?.updateDisplayType({ displayType: serverWidget.FieldDisplayType.INLINE });
                 // CREATE
                 if (type === 'create') {
-                    const isValidForm = VALID_FORMS.has(customFormId);
+                    const isValidForm = formMap.validForms.has(customFormId);
                     core.logDebug('UE', `create | formValid=${isValidForm}`);
                     if (!isValidForm) {
                         hideAllInfrastructureFields(form);
                         return;
                     }
-                    handleCustomForm(rec, customFormId);
+                    handleCustomForm(rec, customFormId ,formMap);
                     return;
                 }
                 // EDIT / VIEW
                 if (type === 'edit' || type === 'view') {
                     core.logDebug('UE', `${type} mode`);
-                    enforceCorrectForm(context, rec);
+                    enforceCorrectForm(context, rec, formMap);
                 }
             } catch (e) {
                 core.logDebug('UE ERROR', e.message);
